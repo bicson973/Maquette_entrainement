1d9561b209937a099c73b26988c191b156854e7c
Bicson STG <87656265+bicson973@users.noreply.github.com>
2021-11-24 15:22:53 +0100

    suite

--- 3 - Cours JAVASCRIPT/B - Variables constantes/js/O3_types_de_donnees.js
+++ 3 - Cours JAVASCRIPT/B - Variables constantes/js/O3_types_de_donnees.js
@@ -1,3 +1,25 @@
 // MA FEUILLE JS - VARIABLES & CONSTANTE - 03 LES TYPES DE DONNEES - SEMA
 
 // alert("le ficher fonctionne");
+
+let string = "String est une chaine de caractère";
+console.log(typeof string, string);
+
+let number = 24; 
+console.log(typeof number, number)
+
+let boolean = true; 
+console.log(typeof boolean, boolean);
+
+let array = ["je", "suis", 24, false]; 
+console.log(typeof array, array);
+
+let object = {
+    prenom : "sema",
+    age: 34, 
+    ville: "Paris",
+}; 
+console.log(typeof object, object);
+
+let arbre; 
+console.log(typeof arbre, arbre);
